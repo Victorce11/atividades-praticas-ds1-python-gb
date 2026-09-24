83adf294a6702c20eb222ba3ff52f29bc1972981
Victor Borba - ds1 <victor.victal.borba@escola.pr.gov.br>
2026-09-24 11:41:55 -0300

    Create ex02.kt

---
+++ kotlin/ex02.kt
@@ -0,0 +1,12 @@
+fun main() {
+  val nome = "Victor"
+  val idade = 17
+  val cidade = Paranaguá
+  val linguagemFavorita = "Python" //* também */
+
+  println("APRESENTAÇÂO PESSOAL")
+  println("Nome: $nome")
+  println("Idade: $idade anos")
+  println("Cidade: $cidade")
+  println("Linguagme favorita: $linguagemFavorita")
+}
