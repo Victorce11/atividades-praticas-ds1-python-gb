1db06924ace475ef99169f475f6de507895988b1
Victor Borba - ds1 <victor.victal.borba@escola.pr.gov.br>
2026-09-24 11:39:05 -0300

    Create ex01.kt

---
+++ kotlin/ex01.kt
@@ -0,0 +1,9 @@
+  fun main() {
+      println{"===================")
+      println("MEU PRIMEIRO PROGRAMA")
+      println("===================")
+      println("Nome: Victor")
+      println("Turma: 1DS")
+      println("Escola: Alberto Gomes Veiga")
+      println("Bem-vindo ao Kotlin!")
+}
